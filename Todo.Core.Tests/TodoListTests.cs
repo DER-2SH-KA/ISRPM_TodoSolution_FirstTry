12eecd9db7a310153daf204d5a57a5d075d22b29
DER-2SH-KA <KurumiForWeb@yandex.ru>
2025-11-22 15:50:41 +0700

    feat: add JSON Save and Load + Tests

--- Todo.Core.Tests/TodoListTests.cs
+++ Todo.Core.Tests/TodoListTests.cs
@@ -12,27 +12,52 @@ namespace Todo.Core.Tests
         public void Add_IncreasesCount()
         {
             var list = new TodoList();
+
             list.Add(" task ");
+
             Assert.Equal(1, list.Count);
             Assert.Equal("task", list.Items.First().Title);
         }
+
         [Fact]
         public void Remove_ById_Works()
         {
             var list = new TodoList();
+
             var item = list.Add("a");
+
             Assert.True(list.Remove(item.Id));
             Assert.Equal(0, list.Count);
         }
+
         [Fact]
         public void Find_ReturnsMatches()
         {
             var list = new TodoList();
+
             list.Add("Buy milk");
             list.Add("Read book");
+
             var found = list.Find("buy").ToList();
+
             Assert.Single(found);
             Assert.Equal("Buy milk", found[0].Title);
+        }
+
+        [Fact]
+        public void SaveAndLoad_ShouldSaveTasksInJson()
+        {
+            var list = new TodoList();
+            var testList = new TodoList();
+
+            list.Add("Buy milk");
+            list.Add("Read book");
+
+            list.Save("\"C:\\Users\\remfo\\Desktop\"\\TodoList.json");
+
+            testList.Load("\"C:\\Users\\remfo\\Desktop\"\\ToDoList.json");
+
+            Assert.True(list.Count == 2);
         }
     }
 }
